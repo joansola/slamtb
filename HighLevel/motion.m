72d4d038c47f363f327209db97734604facdafd0
Joan Sola <jsola@iri.upc.edu>
2009-03-20 15:16:41 +0000

    doc improved

--- HighLevel/motion.m
+++ HighLevel/motion.m
@@ -12,21 +12,20 @@ switch Rob.motion
 
     % const velocity
     case  {'constVel'}
+        
         Rob.state.x = constVel(Rob.state.x,Con.u,dt);
-
         Rob.frame.x = Rob.state.x(1:7);
         Rob.vel.x   = Rob.state.x(8:13);
         Rob.frame   = updateFrame(Rob.frame);
 
         % other motion type:
     case  {'odometry'}
+        
         Rob.frame   = odo3(Rob.frame,Con.u);
-        Rob.frame   = updateFrame(Rob.frame);
-
 
     otherwise
         % TODO : print an error and go out
-        error(['The robot motion model is unknown, cannot move the robot ',Rob_i.name,' with motion type:',Rob_i.motion,'!']);
+        error('??? Unknown motion model ''%s''.',Rob.motion);
 end
 
 

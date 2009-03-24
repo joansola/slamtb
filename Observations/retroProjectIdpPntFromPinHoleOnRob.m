dd2007ac79895d08ecf20e0431fd5b8c4d5b587e
Jean-Marie Codol <jean.marie.codol@gmail.com>
2009-03-24 09:03:09 +0000

    retroProjectIdpPntFromPinHoleOnRob changed her name (old name retroProjectIdpPntIntoPinHoleOnRob

---
+++ Observations/retroProjectIdpPntFromPinHoleOnRob.m
@@ -0,0 +1,15 @@
+
+
+function lmk=retroProjectIdpPntFromPinHoleOnRob(Rf, Sf, Sk, Sd, point)
+
+% RETROPROJECTEUCLPNTINTOPINHOLEONROB De-Project idp point into Pin-hole
+% camera model on robot.
+%
+% TODO implement-it !
+    
+lmk = [] ;
+
+
+end
+
+

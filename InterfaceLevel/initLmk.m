40dc067b86203cb0047b53152434222b3e995043
David Marquez <dmarquez>
2009-04-08 15:50:54 +0000

    create function initLmk.m with old_initLmk and initNewLmks, suprimed initNewLmks.m

--- InterfaceLevel/initLmk.m
+++ InterfaceLevel/initLmk.m
@@ -1,5 +1,19 @@
 function [Lmk,Obs] = initLmk(Rob, Sen, Raw, Lmk, Obs)
 
+%INITLMK  Initialise some new landmarks from recent observations.
+%   [LMK, OBS] = INITNLMK(ROB, SEN, RAW, LMK, OBS) returns the new set of
+%   landmarks.
+%
+%   This "new set" contains the "old set" plus new elements. These new
+%   elements are extracted from the recent observations (RAW), from the
+%   current state estimated (ROB for robot and SEN for the sensor).
+%
+%   Finally, we can have a mean and a variance-covariance estimation for
+%   the new landmark state.
+%
+
+global Map
+
 switch Sen.type
     
     % camera pinHole
@@ -47,20 +61,45 @@ switch Sen.type
                 
                 % INIT LMK
                 inv_depth_nob = Lmk(lmk).nom.n ;
-                [idp, IDPrf, IDPsf, IDPsk, IDPsd, IDPpix, IDPrho] = retroProjectIdpPntFromPinHoleOnRob(Rob.frame, Sen.frame, Sen.par.k, Sen.par.d, y, inv_depth_nob) ;
+                [idp, IDPrf, IDPsf, IDPsk, IDPsd, IDPpix, IDPrho] = ...
+                    retroProjectIdpPntFromPinHoleOnRob( ...
+                    Rob.frame, ...
+                    Sen.frame, ...
+                    Sen.par.k, ...
+                    Sen.par.d, ...
+                    y, ...
+                    inv_depth_nob) ;
                 
-                Lmk(lmk).id   = newId;
-                Lmk(lmk).used = true;
-                Lmk(lmk).state.x  = idp ;
-                Lmk(lmk).sig  = newId;
+                
+                
+                Lmk(lmk).id      = newId;
+                Lmk(lmk).used    = true;
+                Lmk(lmk).state.x = idp ;
+                Lmk(lmk).sig     = newId;
                 
                 
                 % TODO put better variance-covariance matrix
-                Lmk(lmk).state.P  = IDPpix*Sen.par.pixCov*IDPpix' + IDPrho*Lmk(lmk).nom.N*IDPrho' ; % + ... ???
+                Rpix = Sen.par.pixCov ;
+                Rnob = Lmk(lmk).nom.N ;
+                % if the sensor frame is in the state
+                % IDPmap = [IDPrf IDPsf] ;
+                IDPmap = IDPrf ;
+                % if the sensor frame is in the state
+                % Rmap = Map.P([Rob.frame.r;Sen.frame.r],[Rob.frame.r;Sen.frame.r]) ;
+                Rmap = Map.P(Rob.frame.r,Rob.frame.r) ;
+                
+                % var_covar LL
+                Lmk(lmk).state.P  = ...
+                    IDPpix*Rpix*IDPpix' + ...  % by pixel cov
+                    IDPrho*Rnob*IDPrho' + ...  % by nob   cov
+                    IDPmap*Rmap*IDPmap'     ;  % by map   cov
+                
+                % covar_LX
+                P_RX = Map.P(Rob.frame.r,find(Map.used)) ;
+                P_LX = IDPrf*P_RX ;
                 
                 % frame range in Map
-                Lmk(lmk).state.r = addToMap(Lmk(lmk).state.x,Lmk(lmk).state.P);
-                
+                Lmk(lmk).state.r = addToMap(Lmk(lmk).state.x,Lmk(lmk).state.P,P_LX);
             end
         end
         

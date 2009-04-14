43420ac547f396ed1a46e28eb04128898dec425f
Joan Sola <jsola@iri.upc.edu>
2009-04-14 12:47:53 +0000

    pi2pc moved to rotations/. Arrangement of some Contents.m files

--- FrameTransforms/Contents.m
+++ FrameTransforms/Contents.m
@@ -1,25 +1,29 @@
-% FRAMETRANSFORMS
+% Frame transformations.
 %
-% Files
+% Frames
+%   frame         - Help on frames for the FrameTransforms/ toolbox.
+%   getTR         - Get translation vector and rotation matrix
+%   splitFrame    - Split frame information into useful matrices and vectors.
+%   splitInvFrame - Split frame information into useful matrices and vectors.
 %   composeFrames - Compose two 3D frames.
 %   epose2qpose   - Euler-specified to quaternion-specified pose conversion.
-%   frame         - Help on frames for the FrameTransforms/ toolbox.
+%   qpose2epose   - Quaternion-specified to Euler-specified pose conversion.
+%   updateFrame   - Update frame structure.
+%
+% 3D Euclidean points
 %   fromFrame     - Express in global frame a set of points from a local frame.
+%   toFrame       - Express in local frame a set of points from global frame.
+%   Rp            - Rotation matrix (from quaternion) times vector.
+%   Rtp           - Transposed rotation matrix (from quaternion) times vector.
+%   frame2iMatrix - Frame to motion matrix.
+%   frame2matrix  - Frame to motion matrix.
+%
+% Inverse depth points
 %   fromFrameIdp  - Transforms IDP from local frame to global frame.
-%   getTR         - Get translation vector and rotation matrix
+%   toFrameIdp    - Transforms IDP from global frame to local frame.
 %   idp2p         - Inverse Depth to cartesian point conversion.
 %   pi2pc         - Pi matrix to conjugated Pi matrix conversion.
 %   py2vec        - Pitch and yaw to 3D direction vector.
-%   qpose2epose   - Quaternion-specified to Euler-specified pose conversion.
-%   Rp            - Rotation matrix (from quaternion) times vector.
-%   Rtp           - Transposed rotation matrix (from quaternion) times vector.
-%   splitFrame    - Split frame information into useful matrices and vectors.
-%   toFrame       - Express in local frame a set of points from global frame.
-%   toFrameIdp    - Transforms IDP from global frame to local frame.
-%   updateFrame   - Update frame structure.
 %   p2idp         - Point to inverse-depth point conversion.
 %   vec2py        - 3D vector to pitch and yaw direction
 %   idpS2idpW     - transpose idp vector from "Sensor frame" to "World frame".
-%   frame2iMatrix - Frame to motion matrix.
-%   frame2matrix  - Frame to motion matrix.
-%   splitInvFrame - Split frame information into useful matrices and vectors.
--- FrameTransforms/rotations/Contents.m
+++ FrameTransforms/rotations/Contents.m
@@ -1,25 +1,29 @@
-% ROTATIONS
+% Rotations.
 %
-% Files
+% Help
+%   quaternion  - Help on quaternions for the rotations/ toolbox.
+%   eulerAngles - Help on Euler angles for the rotations/ toolbox.
+%
+% Conversion between rotation specifications
 %   au2q       - Rotated angle and rotation axis vector to quaternion.
 %   e2q        - Euler angles to quaternion conversion.
 %   e2R        - Euler angles to rotation matrix.
-%   flu2rdf    - Camera body to camera sensor rotation matrix.
 %   q2au       - quaternion to rotated angle and rotation axis vector.
 %   q2e        - Quaternion to Euler angles conversion.
 %   q2eG       - Transform quaternion Gaussian to Euler Gaussian.
-%   q2Pi       - Pi matrix construction from quaternion.
 %   q2Q        - Quaternion to quaternion matrix conversion.
-%   q2qc       - Conjugate quaternion.
 %   q2R        - Quaternion to rotation matrix conversion.
 %   q2v        - Quaternion to rotation vector conversion.
-%   qProd      - Quaternion product.
-%   quaternion - Help on quaternions for the rotations/ toolbox.
 %   R2e        - Rotation matrix to Euler angles conversion.
 %   R2q        - rotation matrix to quaternion conversion.
 %   v2au       - Rotation vector to rotation axis and angle conversion.
 %   v2q        - Rotaiton vector to quaternion conversion.
+%
+% Other
+%   flu2rdf    - Camera body to camera sensor rotation matrix.
+%   q2Pi       - Pi matrix construction from quaternion.
+%   q2qc       - Conjugate quaternion.
+%   qProd      - Quaternion product.
 %   w2omega    - Skew symetric matrix OMEGA from angular rates vector W.
-%   eulerAngles - Help on Euler angles for the rotations/ toolbox.
-%   qRot        - Vector rotation via quaternion algebra.
+%   qRot       - Vector rotation via quaternion algebra.
 

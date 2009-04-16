92441965b2fe09367b945f0c6f000824f09cfd8f
David Marquez <dmarquez>
2009-04-16 16:30:16 +0000

    added correctLmk.m

--- InterfaceLevel/correctLmk.m
+++ InterfaceLevel/correctLmk.m
@@ -2,21 +2,35 @@ function [Rob,Sen,Lmk,Obs] = correctLmk(Rob,Sen,Lmk,Obs)
 
 global Map
 
-% Pxrsl = Map.P(Map.used,rslr)
-% Hrsl  = [jacobianas jac jac]
-% 
-% x == Map.x(Map.used)
-% P == Map.P(Map.used,Map.used)
-% 
-% K = Pxrsl*Hrsl'*iZ
-% x = x + K*z
-% P = P - K*Z*K'
-% 
-% Rob = map2rob(Rob)
-% Sen = map2sen(Sen)
-%
+% get landmark range
+lr = Lmk.state.r ;        % lmk range in Map
+
+% Rob-Sen-Lmk range and Jacobian
+if Sen.frameInMap
+    rslr = [Rob.frame.r ; Sen.frame.r ; lr]; % range of robot, sensor, and landmark
+    jac  = [Obs.Jac];
+    Hrsl = [jac.E_r jac.E_s jac.E_l];
+    
+else
+    rslr = [Rob.frame.r ; lr]; % range of robot and landmark
+    jac  = [Obs.Jac];
+    Hrsl = [jac.E_r jac.E_l];
+end
+
+Pxrsl = Map.P(Map.used,rslr);
+
+K     = Pxrsl*Hrsl'*Obs.inn.iZ;
+
+Map.x(Map.used)          = Map.x(Map.used) + K*Obs.inn.z;
+Map.P(Map.used,Map.used) = Map.P(Map.used,Map.used) - K*Obs.inn.Z*K';
+
+Rob = map2rob(Rob);
+Sen = map2sen(Sen);
+
+Obs.updated = true;
+
 % Update Obs and Lmk structures.
 
-% TODO: all the function
+
 
 

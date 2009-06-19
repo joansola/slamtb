5ce305f213514074b1f6bc81c331784180c11d59
Joan Sola <jsola@iri.upc.edu>
2009-06-19 09:36:41 +0000

    Added slamrc.m with path definition for the Toolbox. Added functions for 2D segment manipulations.

---
+++ Lines/seg2hmgLin.m
@@ -0,0 +1,16 @@
+function [hm,HMs] = seg2hmgLin(s)
+
+% SEG2HMGLIN  Segment to homogeneous line conversion.
+%   SEG2HMGLIN(S) converts the segment S=[P;Q], with P and Q two Euclidean
+%   points, into an homogeneous line in the plane.
+%
+%   [hm,HMs] = SEG2HMGLIN(S) returns the Jacobians wrt S.
+
+p = s(1:2);
+q = s(3:4);
+if nargout == 1
+    hm = pp2hmgLin(p,q);
+else 
+    [hm,HMp,HMq] = pp2hmgLin(p,q);
+    HMs = [HMp HMq];
+end

1dcf5a8857d00a2483dcf00fdb94ef5d571ca922
Joan Sola <jsola@iri.upc.edu>
2009-08-12 16:05:32 +0000

    normAngle.m accepts now angles in degrees as option

--- Math/normAngle.m
+++ Math/normAngle.m
@@ -1,16 +1,30 @@
-function ao = normAngle(ai)
+function ao = normAngle(ai,deg)
 
 % NORMANGLE Normalize angle to (-pi .. pi] values.
+%   NORMANGLE(A) brings angle A to the range (-pi .. pi] by adding an
+%   integer number of turns.
+%   
+%   NORMANGLE(A,DEG) with DEG ~= false, uses degrees instead of radians,
+%   and thus the angle A is normalized to the range (-180 .. 180].
 
 %   Copyright 2008-2009 Joan Sola @ LAAS-CNRS.
 
+if nargin < 2
+    deg = false;
+end
+
+if deg
+    halfTurn = 180;
+else
+    halfTurn = pi;
+end
 
 ao = ai;
-while any(ao <= -pi)
-    ao(ao <= -pi) = ao(ao <= -pi) + 2*pi;
+while any(ao <= -halfTurn)
+    ao(ao <= -halfTurn) = ao(ao <= -halfTurn) + 2*halfTurn;
 end
-while any(ao > pi)
-    ao(ao > pi) = ao(ao > pi) - 2*pi;
+while any(ao > halfTurn)
+    ao(ao > halfTurn) = ao(ao > halfTurn) - 2*halfTurn;
 end
 
 

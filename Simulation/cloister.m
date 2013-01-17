68c1d070df73d17a4c4f0f94e179f466ab845473
Joan Sola <jsola@iri.upc.edu>
2013-01-17 21:34:00 +0000

    Bug fix in cloister.m

--- Simulation/cloister.m
+++ Simulation/cloister.m
@@ -18,14 +18,14 @@ end
 x0 = (xmin+xmax)/2;
 y0 = (ymin+ymax)/2;
 
-% SIze of cloister
+% Size of cloister
 hsize = xmax-xmin;
 vsize = ymax-ymin;
 tsize = diag([hsize vsize]);
 
 % Integer ordinates of points
 outer = (-(n-3)/2 : (n-3)/2);
-inner = (-(n-3)/2 : (n-1)/2);
+inner = (-(n-3)/2 : (n-5)/2);
 
 % Outer north coordinates
 No = [outer; (n-1)/2*ones(1,numel(outer))];
@@ -42,36 +42,4 @@ f = tsize*points/(n-1);
 % Move
 f(1,:) = f(1,:) + x0;
 f(2,:) = f(2,:) + y0;
-
-
-
-% ========== End of function - Start GPL license ==========
-
-
-%   # START GPL LICENSE
-
-%---------------------------------------------------------------------
-%
-%   This file is part of SLAMTB, a SLAM toolbox for Matlab.
-%
-%   SLAMTB is free software: you can redistribute it and/or modify
-%   it under the terms of the GNU General Public License as published by
-%   the Free Software Foundation, either version 3 of the License, or
-%   (at your option) any later version.
-%
-%   SLAMTB is distributed in the hope that it will be useful,
-%   but WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%   GNU General Public License for more details.
-%
-%   You should have received a copy of the GNU General Public License
-%   along with SLAMTB.  If not, see <http://www.gnu.org/licenses/>.
-%
-%---------------------------------------------------------------------
-
-%   SLAMTB is Copyright 2007,2008,2009 
-%   by Joan Sola, David Marquez and Jean Marie Codol @ LAAS-CNRS.
-%   See on top of this file for its particular copyright.
-
-%   # END GPL LICENSE
 

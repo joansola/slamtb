6bacca8e345be3a41a99fc1222b2aa59fec8d162
Joan Sola <jsola@iri.upc.edu>
2013-02-22 14:28:56 +0000

    Removed GPL license from all files. Need to remove COPYING.txt file to complete

--- HighLevel/slamtbdump.m
+++ HighLevel/slamtbdump.m
@@ -39,8 +39,8 @@ global Map          %#ok<NUSED>
 
 %% I. Specify user-defined options - EDIT USER DATA FILE userDataImg.m
 
-userDataDump;           % user-defined data. SCRIPT.
-% userDataDumpCyril;           % user-defined data. SCRIPT.
+% userDataDump;           % user-defined data. SCRIPT.
+userDataDumpCyril;           % user-defined data. SCRIPT.
 
 
 %% II. Initialize all data structures from user-defined data in userData.m
@@ -249,33 +249,9 @@ end
 % figure(10)
 % plot(Tim.firstFrame:Tim.lastFrame,elapsedTime)
 
-% ========== End of function - Start GPL license ==========
 
 
-%   # START GPL LICENSE
 
-%---------------------------------------------------------------------
-%
-%   This file is part of SLAMTB, a SLAM toolbox for Matlab.
-%
-%   SLAMTB is free software: you can redistribute it and/or modify
-%   it under the terms of the GNU General Public License as published by
-%   the Free Software Foundation, either version 3 of the License, or
-%   (at your option) any later version.
-%
-%   SLAMTB is distributed in the hope that it will be useful,
-%   but WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%   GNU General Public License for more details.
-%
-%   You should have received a copy of the GNU General Public License
-%   along with SLAMTB.  If not, see <http://www.gnu.org/licenses/>.
-%
-%---------------------------------------------------------------------
 
-%   SLAMTB is Copyright 2007,2008,2009
-%   by Joan Sola, David Marquez and Jean Marie Codol @ LAAS-CNRS.
-%   See on top of this file for its particular copyright.
 
-%   # END GPL LICENSE
 

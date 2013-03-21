a40d1808bb8d765a8fe2515d3a8a86591924c397
Joan Sola <jsola@iri.upc.edu>
2013-03-21 17:27:44 +0000

    Add comments for landmark deletion due to negative depth

--- Slam/smartDeleteLmk.m
+++ Slam/smartDeleteLmk.m
@@ -16,14 +16,17 @@ global Map
 switch Lmk.type
     case {'idpPnt','ahmPnt'}
         if Map.x(Lmk.state.r(end)) < 0
+            fprintf('Deleted negative-depth landmark ''%d''.\n',Lmk.id)
             [Lmk,Obs] = deleteLmk(Lmk,Obs);
         end
     case {'idpLin'}
         if any(Map.x(Lmk.state.r([6,9])) < 0)
+            fprintf('Deleted negative-depth landmark ''%d''.\n',Lmk.id)
             [Lmk,Obs] = deleteLmk(Lmk,Obs);
         end
     case {'ahmLin'}
         if any(Map.x(Lmk.state.r([7,11])) < 0)
+            fprintf('Deleted negative-depth landmark ''%d''.\n',Lmk.id)
             [Lmk,Obs] = deleteLmk(Lmk,Obs);
         end
 end

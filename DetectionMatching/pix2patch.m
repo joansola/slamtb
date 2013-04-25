edb335e555c9fcb7d02718050a334d84ed46b274
Joan Sola <jsola@iri.upc.edu>
2013-04-25 11:06:03 +0000

    Changed global image name from Image to Img for coherence with other high-level variables in slamtb

--- DetectionMatching/pix2patch.m
+++ DetectionMatching/pix2patch.m
@@ -6,7 +6,7 @@ function ptch = pix2patch(I,pix,hsize,vsize)
 %   specified, a square patch HSIZExHSIZE is considered.
 %
 %   PATCH = PIX2PATCH(IMID,PIX,HSIZE,VSIZE)  extracts from the global image
-%   Image{IMID} the patch of size HSIZExVSIZE centered at pixel PIX. IMID
+%   Img{IMID} the patch of size HSIZExVSIZE centered at pixel PIX. IMID
 %   is an integer. If VSIZE is not specified, a square patch HSIZExHSIZE is
 %   considered
 %
@@ -14,34 +14,36 @@ function ptch = pix2patch(I,pix,hsize,vsize)
 %     I    : matrix with all pixels - the patch itself
 %     SI   : sum of all patch pixels;
 %     SII  : sum of all squared pixels
-%     bias : sub-pixellic bias
 
-global Image
+global Img
     
 if isscalar(I)  % ndims(I) == 1
     imId = I;
 else
-    Image{1} = I;
+    Img{1} = I;
     imId  = 1;
 end
 
 if nargin < 4
     vsize = hsize;
 end
+
+% minimum necessary margins from central pixel to image edges
 h2 = (hsize-1)/2;
 v2 = (vsize-1)/2;
 
-pix0 = round(pix);
+pix0 = round(pix); % in case the given pixel is not integer
 
-if inSquare(pix,[1+h2 size(Image{imId},2)-h2 1+v2 size(Image{imId},1)-v2])
+if inSquare(pix,[1+h2 size(Img{imId},2)-h2 1+v2 size(Img{imId},1)-v2])
 
-    ptch.I    = single(Image{imId}(pix0(2)-v2:pix0(2)+v2,pix0(1)-h2:pix0(1)+h2));
+    ptch.I    = single(Img{imId}(pix0(2)-v2:pix0(2)+v2,pix0(1)-h2:pix0(1)+h2));
     ptch.SI   = sum(sum(ptch.I));
     ptch.SII  = sum(sum(ptch.I.*ptch.I));
-    ptch.bias = pix-pix0;
 
-else
+else % pixel is too close to the edge: return blank patch
+
     ptch.I = single(zeros(vsize,hsize));
     [ptch.SI,ptch.SII] = deal(0);
     ptch.bias = [0;0];
+
 end

f9207073d34f9aea9337494ee0ad18c731cf9e5f
Joan Sola <jsola@iri.upc.edu>
2015-06-17 16:07:22 +0200

    Compacted frame and motion factors generation into a single function addKeyFrame

--- InterfaceLevel/resetMotion.m
+++ InterfaceLevel/resetMotion.m
@@ -1,7 +1,8 @@
-function Rob = resetMotion(Rob);
+function Rob = resetMotion(Rob)
 
 Rob.state.x = epose2qpose(zeros(6,1));
 Rob.state.P = zeros(7,7);
+
 % ========== End of function - Start GPL license ==========
 
 

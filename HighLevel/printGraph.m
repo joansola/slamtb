03bbdb6cfd581961f83c438a678ae3926322e854
Joan Sola <jsola@iri.upc.edu>
2015-06-22 15:31:27 +0200

    Improved printGraph for better readability

--- HighLevel/printGraph.m
+++ HighLevel/printGraph.m
@@ -3,6 +3,7 @@ function printGraph(Rob,Sen,Lmk,Trj,Frm,Fac)
 global Map
 
 fprintf('--------------------------\n')
+fprintf('Map size: %3d\n', sum(Map.used))
 for rob = [Rob.rob]
     printRob(Rob(rob),1);
     for sen = Rob(rob).sensors
@@ -22,7 +23,6 @@ end
 for lmk = find([Lmk.used])
     printLmk(Lmk(lmk),1);
 end
-fprintf('Map size: %3d\n', sum(Map.used))
 end
 
 
@@ -46,13 +46,13 @@ function printSen(Sen,ntabs)
 fprintf('%sSen: %2d\n', tabs(ntabs), Sen.sen)
 end
 function printLmk(Lmk,ntabs)
-fprintf('%sLmk: %2d, id: %3d\n', tabs(ntabs), Lmk.lmk, Lmk.id)
+fprintf('%sLmk: %2d (%2d)\n', tabs(ntabs), Lmk.lmk, Lmk.id)
 end
 function printTrj(Trj,ntabs)
 fprintf('%sTrj: head <- %s <-tail\n', tabs(ntabs), num2str(mod((Trj.head:-1:Trj.head-Trj.length+1)-1,Trj.maxLength)+1))
 end
 function printFrm(Frm,ntabs)
-fprintf('%sFrm: %2d, id: %3d\n', tabs(ntabs), Frm.frm, Frm.id)
+fprintf('%sFrm: %2d (%3d)\n', tabs(ntabs), Frm.frm, Frm.id)
 end
 function printFac(Fac,ntabs)
 fprintf('%sFac: %3d, %s, ', tabs(ntabs), Fac.fac, Fac.type(1:4))

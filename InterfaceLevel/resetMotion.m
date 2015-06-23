9105be9118d362a0c684af07ef146d2dc77ebd29
Joan Sola <jsola@iri.upc.edu>
2015-06-23 19:04:38 +0200

    Fixed bug leaving wrong information in Rob.frame

--- InterfaceLevel/resetMotion.m
+++ InterfaceLevel/resetMotion.m
@@ -1,6 +1,8 @@
 function Rob = resetMotion(Rob)
 
-Rob.state.x = epose2qpose(zeros(6,1));
+Rob.state.x = vpose2qpose(zeros(6,1));
+Rob.frame.x = Rob.state.x;
+Rob.frame = updateFrame(Rob.frame);
 Rob.state.P = zeros(7,7);
 
 % ========== End of function - Start GPL license ==========

c7c4367f4fc92d6493ca8bfe58adc5bb1653f57f
Joan Sola <jsola@iri.upc.edu>
2015-06-26 18:13:06 +0200

    Improved notation; added comments

--- Slam/computeError.m
+++ Slam/computeError.m
@@ -2,22 +2,47 @@ function [Fac, e, W, J1, J2, r1, r2] = computeError(Rob,Sen,Lmk,Obs,Frm,Fac)
 
 switch Fac.type
     case 'absolute'
+        % Given an expected pose and a pose measurement, compute the error
+        % as the minimal pose that transforms the measurement into the
+        % given pose, that is:
+        %   err = minimal(frameIncrement(meas, expected))
+        % where frameIncrement(F,G) implements the composition of the
+        % inverse of F and G,
+        %   composeFrames(invertFrame(F), G)
+        % and minimal() is a 6 DoF expression of the frame, done using the
+        % function qframe2vframe().
         Fac.exp.e = Frm.state.x;
-        [pq, ~, PQ_e] = frameIncrement(Fac.meas.y, Fac.exp.e); % h(x) (-) y
+        [pq, PQ_y, PQ_e] = frameIncrement(Fac.meas.y, Fac.exp.e); % h(x) (-) y
         [Fac.err.z, Z_pq] = qpose2vpose(pq);
-        Fac.err.E_node1 = Z_pq * PQ_e * Frm.state.M;
-        Fac.err.E_node2 = zeros(6,0);
+        Fac.err.J1 = Z_pq * PQ_e * Frm.state.M; % Jac wrt manifold
+        Fac.err.J2 = zeros(6,0);
+        Z_y = Z_pq * PQ_y;
+        Fac.err.Z = symmetrize(Z_y * Fac.meas.R * Z_y');
+        Fac.err.W = Fac.err.Z^-1;
         Fac.state.r1 = Frm.state.r;
         Fac.state.r2 = [];
         
     case 'motion'
+        % Given two expected poses and a pose increment measurement,
+        % compute the error as the minimal pose that transforms the
+        % measured increment into the increment between the given poses,
+        % that is:
+        %   err = minimal(frameIncrement(meas, frameIncremennt(pose1, pose2)))
+        % where frameIncrement(F,G) implements the composition of the
+        % inverse of F and G,
+        %   composeFrames(invertFrame(F), G)
+        % and minimal() is a 6 DoF expression of the frame, done using the
+        % function qframe2vframe().
         [Fac.exp.e, E_x1, E_x2] = frameIncrement(...
             Frm(1).state.x, ...
             Frm(2).state.x);
-        [pq, ~, PQ_e] = frameIncrement(Fac.meas.y, Fac.exp.e); % h(x) (-) y
+        [pq, PQ_y, PQ_e] = frameIncrement(Fac.meas.y, Fac.exp.e); % h(x) (-) y
         [Fac.err.z, Z_pq] = qpose2vpose(pq);
-        Fac.err.E_node1 = Z_pq * PQ_e * E_x1 * Frm(1).state.M;
-        Fac.err.E_node2 = Z_pq * PQ_e * E_x2 * Frm(2).state.M;
+        Fac.err.J1 = Z_pq * PQ_e * E_x1 * Frm(1).state.M; % Jac wrt manifold 1
+        Fac.err.J2 = Z_pq * PQ_e * E_x2 * Frm(2).state.M; % Jac wrt manifold 2
+        Z_y = Z_pq * PQ_y;
+        Fac.err.Z = symmetrize(Z_y * Fac.meas.R * Z_y');
+        Fac.err.W = Fac.err.Z^-1;
         Fac.state.r1 = Frm(1).state.r;
         Fac.state.r2 = Frm(2).state.r;
 
@@ -25,8 +50,10 @@ switch Fac.type
         Obs = projectLmk(Rob,Sen,Lmk,Obs);
         Fac.exp.e = Obs.exp.e;
         Fac.err.z = Fac.exp.e - Fac.meas.y; % h(x) (-) y
-        Fac.err.E_node1 = Obs.Jac.E_r * Frm.state.M;
-        Fac.err.E_node2 = Obs.Jac.E_l * Lmk.state.M;
+        Fac.err.J1 = Obs.Jac.E_r * Frm.state.M; % Jac wrt manifold 1
+        Fac.err.J2 = Obs.Jac.E_l * Lmk.state.M; % Jac wrt manifold 2
+        Fac.err.Z = Fac.meas.R; % Measurement Jac is negative identity
+        Fac.err.W = Fac.err.Z^-1;
         Fac.state.r1 = Frm.state.r;
         Fac.state.r2 = Lmk.state.r;
     otherwise
@@ -36,8 +63,8 @@ end
 if nargout > 1
     e = Fac.err.z;
     W = Fac.err.W;
-    J1 = Fac.err.E_node1;
-    J2 = Fac.err.E_node2;
+    J1 = Fac.err.J1;
+    J2 = Fac.err.J2;
     r1 = Fac.state.r1;
     r2 = Fac.state.r2;
 end

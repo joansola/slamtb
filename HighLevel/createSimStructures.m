0c13acdb75c1a789de35d8267022e8eeecf4ac66
Joan Sola <jsola@iri.upc.edu>
2015-06-30 10:02:43 +0200

    Updated random generator seed method. Now randn and randi use the same seed and it makes it fully predictable

--- HighLevel/createSimStructures.m
+++ HighLevel/createSimStructures.m
@@ -7,14 +7,16 @@ function [SimRob,SimSen,SimLmk,SimOpt] = createSimStructures(Robot,Sensor,World,
 % Random generator
 if SimOpt.random.newSeed
     SimOpt.random.seed = sum(100*clock);
-    randn('state',SimOpt.random.seed);
+    rng(SimOpt.random.seed);
+    %    randn('state',SimOpt.random.seed);
     fprintf('Random seed: %6.0f.\n',SimOpt.random.seed)
     disp('To repeat this run, edit userData.m,')
     disp('   add this seed to SimOpt.random.fixedSeed,')
     disp('   and set SimOpt.random.newSeed to false.')
 else
     SimOpt.random.seed = SimOpt.random.fixedSeed;
-    randn('state',SimOpt.random.seed);
+    rng(SimOpt.random.seed);
+    %    randn('state',SimOpt.random.seed);
     fprintf('Fixed random seed: %6.0f.\n',SimOpt.random.seed)
     disp('To make further runs truly random, edit userData.m,')
     disp('   and set SimOpt.random.newSeed to true.')

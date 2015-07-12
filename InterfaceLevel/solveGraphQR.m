5d3d77b53d6d20ea6849584f148fafc3100d8087
Joan Sola <jsola@iri.upc.edu>
2015-07-12 13:37:04 +0200

    Added QR method. It works.

---
+++ InterfaceLevel/solveGraphQR.m
@@ -0,0 +1,174 @@
+function [Rob,Sen,Lmk,Obs,Frm,Fac] = solveGraphQR(Rob,Sen,Lmk,Obs,Frm,Fac,options)
+
+% SOLVEGRAPHQR Solves the SLAM graph using QR decomposition.
+%   [Rob,Sen,Lmk,Obs,Frm,Fac] = SOLVEGRAPHQR(Rob,Sen,Lmk,Obs,Frm,Fac)
+%   solves the graph-SLAM problem using QR decomposition of the
+%   Hessian matrix. 
+%
+%   IMPORTANT NOTE: This method is illustrative and constitutes the
+%   motivation for this toolbox. One can achieve better performances, both
+%   in computing time and possibly in robustness and accuracy, by using
+%   Matlab's built-in nonlinear optimization tools, such as LSQNONLIN.
+%   
+%   See courseSLAM.pdf in the documentation for details about the QR
+%   decomposition for solving the graph-SLAM problem.
+%
+%   See also SOLVEGRAPHCHOLESKY, ERRORSTATEJACOBIANS, UPDATESTATES,
+%   COMPUTEERROR, COMPUTERESIDUAL, COLAMD, '\', MLDIVIDE, LSQNONLIN.
+
+% Copyright 2015-    Joan Sola @ IRI-UPC-CSIC.
+
+global Map
+
+% Control of iterations and exit conditions
+n_iter      = options.niterations; % exit criterion of number of iterations
+target_dres = options.target_dres; % exit criterion for error variation
+target_res  = options.target_res;  % exit criterion for current residual
+res_old     = 1e10;                   % last iteration's error
+
+% Map range
+mr = find(Map.used);
+
+for it = 1:n_iter
+    
+    fprintf('----------------\nIteration: %d; \n',it)
+
+    
+    % Compute Jacobians for projection onto the manifold
+    [Frm,Lmk] = errorStateJacobians(Frm,Lmk);
+    
+    % Build Hessian A and rhs vector b, in global Map
+    Fac       = buildProblem(Rob,Sen,Lmk,Obs,Frm,Fac);
+    
+    if it == 1 % do this only once:
+        
+        % Column permutation
+        p  = colamd(Map.A(mr,mr))';
+        
+        % Permutated map range
+        pr = mr(p);
+    end
+    
+    % Decomposition
+    [Map.d, Map.R] = qr(Map.A(:,pr), Map.b, 0);
+    
+    % Solve for dx
+    Map.x(pr) = -Map.R\Map.d; % solve for dx;
+    
+    % Update nominal states
+    [Rob,Lmk,Frm] = updateStates(Rob,Lmk,Frm);
+    
+    % Check resulting errors
+    [res, err_max] = computeResidual(Rob,Sen,Lmk,Obs,Frm,Fac);
+    dres           = res - res_old;
+    res_old        = res;
+    
+    fprintf('Residual: %.2e; variation: %.2e \n', res, dres)
+    
+    
+    if ( ( -dres <= target_dres ) || (err_max <= target_res) ) %&& ( abs(derr) < target_derr) )
+        break;
+    end
+    
+end
+
+end
+
+
+function Fac = buildProblem(Rob,Sen,Lmk,Obs,Frm,Fac)
+
+% BUILDPROBLEM Build least squares problem's matrix A and vector b 
+%   Fac = BUILDPROBLEM(Rob,Sen,Lmk,Obs,Frm,Fac) Builds the least squares
+%   problem's matrix A and vector b for a solution using sparse QR
+%   factorization of A.
+
+global Map
+
+% Reset Hessian and rhs vector
+mr = find(Map.used);
+Map.A(:,mr) = 0;
+Map.b(:)    = 0;
+
+% Iterate all factors
+firstLine = 1;
+for fac = find([Fac.used])
+    
+    % Extract some pointers
+    rob    = Fac(fac).rob;
+    sen    = Fac(fac).sen;
+    lmk    = Fac(fac).lmk;
+    frames = Fac(fac).frames;
+    
+    % Compute factor error, info mat, and Jacobians
+    [Fac(fac), e, W, J1, J2, r1, r2] = computeError(...
+        Rob(rob),       ...
+        Sen(sen),       ...
+        Lmk(lmk),       ...
+        Obs(sen,lmk),   ...
+        Frm(frames),    ...
+        Fac(fac));
+    
+    Wsqrt = chol(W);
+    
+    % row band matrix size
+    m  = numel(e);
+    mr = (firstLine : firstLine + m - 1);
+
+    % Compute sparse Hessian blocks
+    A_1 = Wsqrt * J1;
+    A_2 = Wsqrt * J2;
+    
+    % Compute rhs vector blocks
+    b1   = Wsqrt * e;
+    b2   = Wsqrt * e;
+    
+    % Update A and b
+    Map.A(mr,r1) = A_1;
+    Map.A(mr,r2) = A_2;
+    
+    Map.b(mr,1)  = b1;
+    
+    firstLine = firstLine + m;
+
+end
+
+end
+
+
+
+
+% ========== End of function - Start GPL license ==========
+
+
+%   # START GPL LICENSE
+
+%---------------------------------------------------------------------
+%
+%   This file is part of SLAMTB, a SLAM toolbox for Matlab.
+%
+%   SLAMTB is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   SLAMTB is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with SLAMTB.  If not, see <http://www.gnu.org/licenses/>.
+%
+%---------------------------------------------------------------------
+
+%   SLAMTB is Copyright:
+%   Copyright (c) 2008-2010, Joan Sola @ LAAS-CNRS,
+%   Copyright (c) 2010-2013, Joan Sola,
+%   Copyright (c) 2014-2015, Joan Sola @ IRI-UPC-CSIC,
+%   SLAMTB is Copyright 2009
+%   by Joan Sola, Teresa Vidal-Calleja, David Marquez and Jean Marie Codol
+%   @ LAAS-CNRS.
+%   See on top of this file for its particular copyright.
+
+%   # END GPL LICENSE
+

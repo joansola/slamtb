db6b108d552dcb72b77899206aec3c60cf02603d
Ellon Mendes <ellon.mendes@laas.fr>
2015-09-12 01:00:04 +0200

    use vecsAngle to initialize parallax from measurements

--- Points/measurements2pap.m
+++ Points/measurements2pap.m
@@ -21,7 +21,7 @@ vecAtoP = assocampose.R*vecAtoPInCf;
 py = vec2py(vecMtoP);
 
 % get parallax angle from the vectors from anchors to landmark
-par = angleBetweenVectors(vecMtoP,vecAtoP);
+par = vecsAngle(vecMtoP,vecAtoP);
 
 % compose the pap pnt
 pap(1:3,1) = maincampose.t;
@@ -29,14 +29,4 @@ pap(4:6,1) = assocampose.t;
 pap(7:8,1) = py;
 pap(9,1)   = par;
 
-end
-
-function angle = angleBetweenVectors(v1,v2)
-% ANGLEBETWEENVECTORS Return the angle between two vectors
-%
-% ANGLE = ANGLEBETWEENVECTORS(V1,V2) returns the angle between V1 and V2.
-% ANGLE has ranges from 0 to pi.
-
-% TODO: which one is better: 0 <= par <= pi or -pi/2 <= par <= pi/2?
-angle = acos( dot(v1,v2) / (norm(v1)*norm(v2)) );
 end

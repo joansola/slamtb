a24df1d3bd087a404fba036c8441891525793321
Joan Solà <jsola@iri.upc.edu>
2017-03-01 22:56:35 +0100

    Make exact method the default. Rename tustin to euler

--- Kinematics/qpredict.m
+++ Kinematics/qpredict.m
@@ -11,10 +11,10 @@ function [q1,Q1_q,Q1_w] = qpredict(q,w,dt,met)
 %
 %   [...] = QPREDICT(...,MET) allows the specification of the method to
 %   update the quaternion:
-%       'tustin' uses Qu = Q + .5*DT*W2OMEGA(W)*Q
+%       'euler' uses Qu = Q + .5*DT*W2OMEGA(W)*Q
 %       'exact'  uses Qu = QPROD(Q,V2Q(W*DT))
-%   The Jacobians are always computed according to the 'tustin' method.
-%   'tustin' is the default method.
+%   The Jacobians are always computed according to the 'euler' method.
+%   'exact' is the default method.
 %
 %   See also QPROD, W2OMEGA, Q2PI, V2Q, QUATERNION.
 
@@ -23,28 +23,28 @@ function [q1,Q1_q,Q1_w] = qpredict(q,w,dt,met)
 switch nargin
     case 2
         dt = 1;
-        met = 'tustin';
+        met = 'exact';
     case 3
         if isa(dt,'char')
             met = dt;
             dt = 1;
         else
-            met = 'tustin';
+            met = 'exact';
         end
 end
 
 
 switch lower(met)
-    case 'tustin'
+    case 'euler'
         W  = w2omega(w);
-        q1 = q + .5*dt*W*q; % Tustin integration - fits with Jacobians
+        q1 = q + .5*dt*W*q; % euler integration - fits with Jacobians
     case 'exact'
-        q1 = qProd(q,v2q(w*dt)); % True value - Jacobians based on tustin form
+        q1 = qProd(q,v2q(w*dt)); % True value - Jacobians based on euler form
     otherwise
-        error('Unknown quaternion predict method. Use ''tustin'' or ''exact''')
+        error('Unknown quaternion predict method. Use ''euler'' or ''exact''')
 end
 
-if nargout > 1  % Jacobians always use Tustin method
+if nargout > 1  % Jacobians always use euler method
     W    = w2omega(w);
     Q1_q = eye(4) + 0.5*dt*W;
     Q1_w = 0.5*dt*q2Pi(q);
